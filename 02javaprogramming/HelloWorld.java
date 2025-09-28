7af12bdeca5ad932a37354c150a2bc6ec2b0a774
shivm-singh <1388shivamsingh@gmail.com>
2025-09-28 23:34:04 +0530

    remove the message again

--- 02javaprogramming/HelloWorld.java
+++ 02javaprogramming/HelloWorld.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class HelloWorld {
     
-    // Your program begins with a call to main().
+
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
